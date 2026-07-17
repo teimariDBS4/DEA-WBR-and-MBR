cfde6c0f60ee5d7edc9fde830c8cedcb9e0845bc
teimariDBS4 <teimari@amazon.com>
2026-07-17 15:22:05 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAY3zDqXgJDe7eS4XKJIbjvZ4q4Hy69gjU",
+  authDomain: "dea-bridge.firebaseapp.com",
+  databaseURL: "https://dea-bridge-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "dea-bridge",
+  storageBucket: "dea-bridge.firebasestorage.app",
+  messagingSenderId: "743587089958",
+  appId: "1:743587089958:web:0382c6512235b8539f20e7"
+};
